54e7ace3d188200b79bdd882bb3f4cdfda22bc74
Tolga Durukan <tolgadurukan16@gmail.com>
2021-03-22 21:03:53 +0200

    fix: listItem key error

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,18 +4,12 @@ import styles from './index.module.css'
 
 interface Props {
   isOpen: boolean
-  key: string
   children: React.ReactNode
   onClick: any
   id: string
 }
 
-const ListItem: React.FC<Props> = ({
-  isOpen = false,
-  key,
-  children,
-  ...props
-}) => {
+const ListItem: React.FC<Props> = ({isOpen = false, children, ...props}) => {
   return (
     <React.Fragment>
       <Cover isOpen={isOpen} />
